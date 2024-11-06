cab11acec6afddcc018413ce0206d5aa1e8237ed
chaejunho689 <smile689@naver.com>
2024-11-06 18:01:53 +0900

    20241106

--- SpringBoot/ThymeleafTest/src/main/java/com/example/demo/ThymeleafTestApplication.java
+++ SpringBoot/ThymeleafTest/src/main/java/com/example/demo/ThymeleafTestApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"Thymeleaf.controller"})
+@ComponentScan(basePackages = {"Thymeleaf.controller", "thymeleaf.bean"})
 @SpringBootApplication
 public class ThymeleafTestApplication {
 
